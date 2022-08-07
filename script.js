d980eb3779003587412663c86abfe6c2b2452f1c
Jonathan Lieu <razerlieu@gmail.com>
2022-08-07 16:10:55 -0700

    added dark mode to badges

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const toggle = document.querySelector('.toggle-icon');
 const body = document.body;
 const cardBody = document.querySelectorAll('.card-body');
 const emailLinkStyle = document.querySelector('.email-link');
+const badgeStyle = document.querySelectorAll('.badge');
 
 toggle.addEventListener('click', () => {
     toggle.classList.toggle('bi-sun');
@@ -16,6 +17,23 @@ toggle.addEventListener('click', () => {
 
     for(i = 0; i < cardBody.length; i++){
         cardBody[i].classList.toggle('dark');
+    }
+
+    for(i = 0; i < badgeStyle.length; i++){
+        badgeStyle[i].classList.toggle('bg-dark');
+
+        if(badgeStyle[i].style.color == "white"){
+            badgeStyle[i].style.color = "black";
+        }else{
+            badgeStyle[i].style.color = "white";
+        }
+
+        if(badgeStyle[i].style.outlineColor == "white"){
+            badgeStyle[i].style.outlineColor = "black";
+        }else{
+            badgeStyle[i].style.outlineColor = "white";
+        }
+
     }
 })
 
